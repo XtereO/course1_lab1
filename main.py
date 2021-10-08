092ce0b8e50c939489477e8df151bc7267390d92
XtereO <71135845+XtereO@users.noreply.github.com>
2021-10-08 13:09:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
 
 import json
-from api import (find_summary_symbols_in_file,
-                            find_summary_symbols_cut_in_file,
-                            find_element_with_key_where_most_symbols,
-                            find_value_in_array_with_key_where_most_words)
+from api import (
+                 find_summary_symbols_in_file,
+                 find_summary_symbols_cut_in_file,
+                 find_element_with_key_where_most_symbols,
+                 find_value_in_array_with_key_where_most_words)
                             
                             
 def main():
