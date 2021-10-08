4f55758ec5a082b4e1776534ba8ba48485629844
XtereO <71135845+XtereO@users.noreply.github.com>
2021-10-08 12:41:42 +0300

    Refactoring

--- logic.py
+++ logic.py
@@ -12,8 +12,56 @@ def validation_key_list(list_objects, key):
         return False
     return True
 
+def validation_emty_elements_list(check_list):
+    if(type('str')==type(check_list)):
+        check_list=check_list.split(' ')
+    if(not('' in check_list) and not(' ' in  check_list)):
+        return True
+    return False
+
 
 #Main functions
+def find_summary_symbols_in_file(file_name):
+    
+    
+    #Validation arguments
+    if(type('str')!=type(file_name)):
+        return 'File name must be string'
+        
+     
+    #Read file and generate answer
+    file = open(file_name, 'r') 
+    
+    summary_symbols = 0
+    for a in file: summary_symbols+=len(a)
+    file.close()
+    
+    
+    #Result
+    return summary_symbols
+
+def find_summary_symbols_cut_in_file(file_name):
+    
+    
+    #Validation arguments
+    if(type('str')!=type(file_name)):
+        return 'File name must be string'
+        
+     
+    #Read file and generate answer
+    file = open(file_name, 'r') 
+    
+    summary_symbols_cut = 0
+    for a in file: 
+        summary_symbols_cut+=len(
+            re.sub(r'[,.;:?!-() ]','',a).replace(' ','')
+        )
+    file.close()
+    
+    
+    #Result
+    return summary_symbols_cut
+
 def find_element_with_key_where_most_symbols(list_objects, key):
 
 
@@ -36,7 +84,7 @@ def find_element_with_key_where_most_symbols(list_objects, key):
     #Result
     return candidates
 
-def find_value_in_array_with_key_where_most_symbols(list_objects, key):
+def find_value_in_array_with_key_where_most_words(list_objects, key):
 
 
     #Validation arguments
@@ -51,11 +99,11 @@ def find_value_in_array_with_key_where_most_symbols(list_objects, key):
     candidates = ['']
     for a in list_objects:
         for i in a[key]:
-            if(len(i) > len(candidates[0])):
+            if(len(i.split(' ')) > len(candidates[0].split(' ')) and validation_emty_elements_list(i)):
                 candidates = [i]
-            elif(len(i) == len(candidates[0]) and not(i in candidates)):
+            elif(len(i.split(' ')) == len(candidates[0].split(' ')) and not(i in candidates) 
+                  and validation_emty_elements_list(i)):
                 candidates.append(i)
     
-
     #Result
     return candidates
--- main.py
+++ main.py
@@ -2,27 +2,35 @@
 
 import json
 import re
-from logic import (find_element_with_key_where_most_symbols,
-                   find_value_in_array_with_key_where_most_symbols)
+from logic import (find_summary_symbols_in_file,
+                            find_summary_symbols_cut_in_file,
+                            find_element_with_key_where_most_symbols,
+                            find_value_in_array_with_key_where_most_words)
+                            
+                            
+def main():
+    
+    
+    #CONSTS
+    FILE_NAME = 'pockemon_full.json'
+    
+    
+    #Read file
+    with open(FILE_NAME, 'r') as file: 
+        pockemon_list = json.load(file) 
+   
+   
+    #Generate answers
+    summary_symbols = find_summary_symbols_in_file(FILE_NAME) 
+    summary_symbols_cut = find_summary_symbols_cut_in_file(FILE_NAME)
+    elements_with_largest_description = find_element_with_key_where_most_symbols(pockemon_list,'description')
+    abilities_with_largest_words = find_value_in_array_with_key_where_most_words(pockemon_list,'abilities') 
 
 
-#Read file
-with open('pockemon_full.json', 'r', encoding='utf-8') as file: 
-    pockemon_list = json.load(file) 
-
-
-#Generate answers
-pockemon_list_str = json.dumps(pockemon_list)
-summary_symbols = (len(pockemon_list_str))
-summary_symbols_cut = len(
-    re.sub(r'[,.;:?!-() ]','',pockemon_list_str).replace(' ','')
-    )
-elements_with_largest_description = find_element_with_key_where_most_symbols(pockemon_list,'description')
-abilities_with_largest_symbols = find_value_in_array_with_key_where_most_symbols(pockemon_list,'abilities') 
-
-
-#Output
-print(f'Summary symbols: {summary_symbols}')
-print(f'Summary symbols without spaces and punctuation marks: {summary_symbols_cut}')
-print(f'Who have a most description: {elements_with_largest_description}')
-print(f'What kind of an abilities most used: {abilities_with_largest_symbols}')
+    #Output
+    print(f'Summary symbols: {summary_symbols}')
+    print(f'Summary symbols without spaces and punctuation marks: {summary_symbols_cut}')
+    print(f'Who have a most description: {elements_with_largest_description}')
+    print(f'What kind of an abilities most used: {abilities_with_largest_words}')
+    
+main()
