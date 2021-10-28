27440670f773e53ae936cb18a9c65350ab710aa8
XtereO <71135845+XtereO@users.noreply.github.com>
2021-10-28 15:13:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,23 +12,22 @@ def main():
     
     
     # Read file
-    file = open(FILE_NAME)
-    pockemon_list = json.load(file) 
+    with open(FILE_NAME) as file:
+      pockemon_list = json.load(file) 
    
    
-    # Generate answers
-    summary_symbols = find_summary_symbols_in_file(file) 
-    summary_symbols_cut = find_summary_symbols_cut_in_file(file)
-    elements_with_largest_description = find_element_with_key_where_most_symbols(pockemon_list,'description')
-    abilities_with_largest_words = find_value_in_array_with_key_where_most_words(pockemon_list,'abilities') 
+      # Generate answers
+      summary_symbols = find_summary_symbols_in_file(file) 
+      summary_symbols_cut = find_summary_symbols_cut_in_file(file)
+      elements_with_largest_description = find_element_with_key_where_most_symbols(pockemon_list,'description')
+      abilities_with_largest_words = find_value_in_array_with_key_where_most_words(pockemon_list,'abilities') 
 
 
-    # Output
-    print(f'Summary symbols: {summary_symbols}')
-    print(f'Summary symbols without spaces and punctuation marks: {summary_symbols_cut}')
-    print(f'Who have a most description: {elements_with_largest_description}')
-    print(f'What kind of an abilities most used: {abilities_with_largest_words}')
+      # Output
+      print(f'Summary symbols: {summary_symbols}')
+      print(f'Summary symbols without spaces and punctuation marks: {summary_symbols_cut}')
+      print(f'Who have a most description: {elements_with_largest_description}')
+      print(f'What kind of an abilities most used: {abilities_with_largest_words}')
     
-    file.close()
     
 main()
